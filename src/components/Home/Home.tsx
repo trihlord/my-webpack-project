6eb18857cf45fd0672177ab28ddfc7f462690149
Vladimir Kubantsev <trihlord@gmail.com>
2023-10-05 23:45:28 +0300

    Feat button and text

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,18 @@
 import webpackLogo from '@/assets/webpack.png'
+import { Button } from '@/lib/Button'
+import { Text } from '@/lib/Text'
 import classes from './Home.module.css'
 
 function Home() {
     return (
         <div className={classes.container}>
-            <h1 className={classes.heading}>Welcome to your Typescript React App!</h1>
+            <Text typography="headline-xl" weight="light">
+                Welcome to your Typescript React App!
+            </Text>
             <img src={webpackLogo} alt="webpack logo" />
+            <Button view="default" size="m">
+                Click me
+            </Button>
         </div>
     )
 }
---
+++ src/lib/Button.ts
@@ -0,0 +1,1 @@
+export * from '@yandex/ui/Button/desktop/bundle'
