6e7e77c19cc1f8120447315c4e77d238afd942d1
Vladimir Kubantsev <trihlord@gmail.com>
2023-12-02 00:55:50 -0800

    Chore paths constants

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Home from './components/Home'
 import Root from './components/Root'
+import { Path } from './constants/path'
 
 const router = createBrowserRouter([
     {
         element: <Root />,
         children: [
             {
-                path: '/',
+                path: Path.HOME,
                 element: <Home />,
             },
         ],
